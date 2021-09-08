404f5fb3bf32baa1f66e6944dd2954f6adb09e2d
Dara Carolan <daraclare@gmail.com>
2021-09-08 16:58:19 +0100

    passport

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
 const sessionsRouter = require('./src/routers/sessionsRouter');
 const adminRouter = require('./src/routers/adminRouter');
 const authRouter = require('./src/routers/authRouter');
@@ -10,11 +14,14 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 
-
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({ secret: 'globomantics' }));
+
+require('./src/config/passport.js')(app);
 
 app.set('views', "./src/views");
 app.set('view engine', 'ejs');
