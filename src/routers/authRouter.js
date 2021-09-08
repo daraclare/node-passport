ffe94cf94430e903f48b9341f482938dfc6eac9a
Dara Carolan <daraclare@gmail.com>
2021-09-08 19:39:54 +0100

    auth  router and  strategy

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -6,9 +6,35 @@ const authRouter = express.Router();
 
 
 authRouter.route('/signUp').post((req, res) => {
+    const { username, password } = req.body;
 
-    res.json(req.body);
+    const url = 'mongodb+srv://daraclare:01234567890@globomantics.zzh0w.mongodb.net?retryWrites=true&w=majority';
+    const dbName = 'globomantics';
 
+    (async function addUser() {
+        let client;
+        try {
+            client = await MongoClient.connect(url);
+            const db = client.db(dbName);
+            const user = { username, password };
+            const results = await db.collection('users').insertOne(user);
+            debug(results);
+
+            console.log("RESULKTS!!!", results);
+
+            req.login(results.ops[0], () => {
+                res.redirect('/auth/profile');
+            });
+        } catch (error) {
+            debug(error);
+        }
+        client.close();
+    }());
+
+});
+
+authRouter.route('/profile').get((req, res) => {
+    res.json(req.user);
 });
 
 module.exports = authRouter;
