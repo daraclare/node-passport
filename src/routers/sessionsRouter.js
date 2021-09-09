d14673ffdbc0022e61a617d9c24b39c64c0377ac
Dara Carolan <daraclare@gmail.com>
2021-09-09 16:05:14 +0100

    authorise user

--- src/routers/sessionsRouter.js
+++ src/routers/sessionsRouter.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const debug = require('debug')('app:sessionRouter');
-const { MongoClient, ObjectId } = require('mongodb');
-const sessionsRouter = express.Router();
+const { MongoClient, ObjectID } = require('mongodb');
 const sessions = require('../data/sessions.json');
 
+const sessionsRouter = express.Router();
+
+sessionsRouter.use((req, res, next) => {
+    if (req.user) {
+        next();
+    } else {
+        res.redirect('/auth/signIn');
+    }
+});
 
 sessionsRouter.route('/').get((req, res) => {
     const url = 'mongodb+srv://daraclare:01234567890@globomantics.zzh0w.mongodb.net?retryWrites=true&w=majority';
@@ -36,7 +44,7 @@ sessionsRouter.route('/:id').get((req, res) => {
             client = await MongoClient.connect(url);
             debug('Connected to the Mongo DB');
             const db = client.db(dbName);
-            const session = await db.collection('sessions').findOne({ _id: new ObjectId(id) });
+            const session = await db.collection('sessions').findOne({ _id: new ObjectID(id) });
             res.render('session', { session });
         } catch (error) {
             debug(error.stack);
